81724097/itop

commit add093466e98edb0bd1a330ff417460f5b503a20

Split the method to get a module absolute URL into 3 different methods, to allow more flexibility (for example get the URL and the query string key/value array to construct a GET form)
git-svn-id: http://svn.code.sf.net/p/itop/code/trunk@5071 a333f486-631f-4898-b8df-5754b55c2be0

application/utils.inc.php
@@ -1064,8 +1064,7 @@ class utils
 	}
 
     /**
-     * Returns the absolute URL to the modules root path
-     * @return string ...
+     * @return string the absolute URL to the modules root path
      */                   
 	static public function GetAbsoluteUrlModulesRoot()
 	{
@@ -1073,33 +1072,67 @@ class utils
 		return $sUrl;
 	}
 
-    /**
-     * Returns the URL to a page that will execute the requested module page
-     *      
-     * To be compatible with this mechanism, the called page must include approot
-     * with an absolute path OR not include it at all (losing the direct access to the page)
-     * if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
-     * require_once(__DIR__.'/../../approot.inc.php');
-     *      
-     * @return string ...
-     */                   
+	/**
+	 * To be compatible with this mechanism, the called page must include approot with an absolute path OR not include
+	 * it at all (losing the direct access to the page) :
+	 *
+	 * ```php
+	 * if (!defined('__DIR__')) define('__DIR__', dirname(__FILE__));
+	 * require_once(__DIR__.'/../../approot.inc.php');
+	 * ```
+	 *
+	 * @param string $sModule
+	 * @param string $sPage
+	 * @param string[] $aArguments
+	 * @param string $sEnvironment
+	 *
+	 * @return string the URL to a page that will execute the requested module page
+	 * @throws \Exception if one of the argument is a reserved one
+	 *
+	 * @see GetExecPageArguments
+	 * @see GetAbsoluteUrlExecPage
+	 */
 	static public function GetAbsoluteUrlModulePage($sModule, $sPage, $aArguments = array(), $sEnvironment = null)
+	{
+		$aArgs = self::GetExecPageArguments($sModule, $sPage, $aArguments, $sEnvironment);
+		$sArgs = http_build_query($aArgs);
+
+		return self::GetAbsoluteUrlExecPage().$sArgs;
+	}
+
+	/**
+	 * @param string $sModule
+	 * @param string $sPage
+	 * @param string[] $aArguments
+	 * @param string $sEnvironment
+	 *
+	 * @return string[] exec.php query string arguments for the specified module
+	 */
+	static public function GetExecPageArguments($sModule, $sPage, $aArguments = array(), $sEnvironment = null)
 	{
 		$sEnvironment = is_null($sEnvironment) ? self::GetCurrentEnvironment() : $sEnvironment;
 		$aArgs = array();
-		$aArgs[] = 'exec_module='.$sModule;
-		$aArgs[] = 'exec_page='.$sPage;
-		$aArgs[] = 'exec_env='.$sEnvironment;
+		$aArgs['exec_module'] = $sModule;
+		$aArgs['exec_page'] = $sPage;
+		$aArgs['exec_env'] = $sEnvironment;
 		foreach($aArguments as $sName => $sValue)
 		{
-			if (($sName == 'exec_module')||($sName == 'exec_page')||($sName == 'exec_env'))
+			if (($sName == 'exec_module') || ($sName == 'exec_page') || ($sName == 'exec_env'))
 			{
 				throw new Exception("Module page: $sName is a reserved page argument name");
 			}
-			$aArgs[] = $sName.'='.urlencode($sValue);
+			$aArgs[$sName] = urlencode($sValue);
 		}
-		$sArgs = implode('&', $aArgs);
-		return self::GetAbsoluteUrlAppRoot().'pages/exec.php?'.$sArgs;
+
+		return $aArgs;
+	}
+
+	/**
+	 * @return string
+	 */
+	static public function GetAbsoluteUrlExecPage()
+	{
+		return self::GetAbsoluteUrlAppRoot().'pages/exec.php';
 	}
 
 	/**

commit 3840dc855a0cdaad5f3372ef857c93ddc09f5a45

Fix regression introduced in r5071 (wooops), plus some PHPDoc
git-svn-id: http://svn.code.sf.net/p/itop/code/trunk@5074 a333f486-631f-4898-b8df-5754b55c2be0

application/utils.inc.php
@@ -1087,9 +1087,8 @@ class utils
 	 * @param string $sEnvironment
 	 *
 	 * @return string the URL to a page that will execute the requested module page
-	 * @throws \Exception if one of the argument is a reserved one
 	 *
-	 * @see GetExecPageArguments
+	 * @see GetExecPageArguments can be used to submit using the GET method (see bug in N.1108)
 	 * @see GetAbsoluteUrlExecPage
 	 */
 	static public function GetAbsoluteUrlModulePage($sModule, $sPage, $aArguments = array(), $sEnvironment = null)
@@ -1097,7 +1096,7 @@ class utils
 		$aArgs = self::GetExecPageArguments($sModule, $sPage, $aArguments, $sEnvironment);
 		$sArgs = http_build_query($aArgs);
 
-		return self::GetAbsoluteUrlExecPage().$sArgs;
+		return self::GetAbsoluteUrlExecPage()."?".$sArgs;
 	}
 
 	/**
@@ -1107,6 +1106,7 @@ class utils
 	 * @param string $sEnvironment
 	 *
 	 * @return string[] exec.php query string arguments for the specified module
+	 * @throws \Exception if one of the argument has a reserved name
 	 */
 	static public function GetExecPageArguments($sModule, $sPage, $aArguments = array(), $sEnvironment = null)
 	{