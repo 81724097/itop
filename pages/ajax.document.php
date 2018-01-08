81724097/itop

commit 8c144c087138e873246752302fca62aa8b53a76d

N°1227 New configuration parameter (disable_attachments_download_legacy_portal) to disable attachments download from the legacy portal. Default is "true"!
git-svn-id: http://svn.code.sf.net/p/itop/code/trunk@5183 a333f486-631f-4898-b8df-5754b55c2be0

pages/ajax.document.php
@@ -53,7 +53,9 @@ try
 	switch($operation)
 	{
 		case 'download_document':
-			LoginWebPage::DoLoginEx(null /* any portal */, false);
+			// Fixing security hole from bug N°1227, disabling by default attachment from legacy portal.
+			$sRequestedPortalId = (MetaModel::GetConfig()->Get('disable_attachments_download_legacy_portal') === true) ? 'backoffice' : null;
+			LoginWebPage::DoLoginEx($sRequestedPortalId, false);
 			$id = utils::ReadParam('id', '');
 			$sField = utils::ReadParam('field', '');
 			if ($sClass == 'Attachment')

core/config.class.inc.php
@@ -962,6 +962,14 @@ class Config
 			'source_of_value' => '',
 			'show_in_conf_sample' => false,
 		),
+		'disable_attachments_download_legacy_portal' => array(
+			'type' => 'bool',
+			'description' => 'Disable attachments download from legacy portal',
+			'default' => true,
+			'value' => true,
+			'source_of_value' => '',
+			'show_in_conf_sample' => true,
+		),
 	);
 
 	public function IsProperty($sPropCode)

commit 6650e741762067ad289e55dc0f8ecda92bc83877

Fix regression introduced in r5183.
git-svn-id: http://svn.code.sf.net/p/itop/code/trunk@5211 a333f486-631f-4898-b8df-5754b55c2be0

pages/ajax.document.php
@@ -54,7 +54,7 @@ try
 	{
 		case 'download_document':
 			// Fixing security hole from bug N°1227, disabling by default attachment from legacy portal.
-			$sRequestedPortalId = (MetaModel::GetConfig()->Get('disable_attachments_download_legacy_portal') === true) ? 'backoffice' : null;
+			$sRequestedPortalId = ((MetaModel::GetConfig()->Get('disable_attachments_download_legacy_portal') === true) && ($sClass === 'Attachment')) ? 'backoffice' : null;
 			LoginWebPage::DoLoginEx($sRequestedPortalId, false);
 			$id = utils::ReadParam('id', '');
 			$sField = utils::ReadParam('field', '');